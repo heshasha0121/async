0df089a41a16e5b611821e7e2088a9eff98d8ac7
lvchuan <409775253@qq.com>
2024-06-13 09:44:39 +0800

    excel导入公共化接口

--- async-trigger/src/main/java/com/lvchuan/config/MQCommonConsumerService.java
+++ async-trigger/src/main/java/com/lvchuan/config/MQCommonConsumerService.java
@@ -2,7 +2,6 @@ package com.lvchuan.config;
 
 import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.lvchuan.common.MqTopicConstant;
 import com.lvchuan.common.aysnc.AsyncAcceptHandler;
 import com.lvchuan.common.aysnc.AsyncMsgDTO;
@@ -13,7 +12,6 @@ import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Objects;
 
 /**
  * @description: mq公共消费者
